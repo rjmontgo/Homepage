763f44665f8532e9893af09573a6250f33580d4e
Robert Montgomery <robert.montgomery@levvel.io>
2020-07-05 23:35:58 -0400

    Update css animations and make the search bar look nicer

--- index.js
+++ index.js
@@ -15,22 +15,24 @@ window.addEventListener("load", function() {
 })
 
 /** Life Progress bar */
-const deathDate = new Date("Feb 23 2076");
-const birthDate = new Date("Feb 23 1997");
+function updateProgressBar() {
+  const deathDate = new Date("Feb 23 2076");
+  const birthDate = new Date("Feb 23 1997");
 
-const totalLife = deathDate - birthDate;
-const spentLife = new Date() - birthDate;
+  const totalLife = deathDate - birthDate;
+  const spentLife = new Date() - birthDate;
 
-const lifeProgress = spentLife / totalLife * 100;
-const days = Math.floor((totalLife - spentLife) / 1000 / 60 / 60 / 24);
+  const lifeProgress = spentLife / totalLife * 100;
+  const days = Math.floor((totalLife - spentLife) / 1000 / 60 / 60 / 24);
 
-const lifeBar = document.getElementById('life-bar');
-const lifeStats = document.getElementById('life-stats');
-lifeBar.setAttribute('days', days);
-lifeBar.setAttribute('progress', lifeProgress);
-lifeStats.innerHTML = 
-  `You have lived approximately ${Math.ceil(spentLife / totalLife * 100)}% of your life`
-
+  const lifeBar = document.getElementById('life-bar');
+  const lifeStats = document.getElementById('life-stats');
+  lifeBar.setAttribute('days', days);
+  lifeBar.setAttribute('progress', lifeProgress);
+  lifeStats.innerHTML = 
+    `You have lived approximately ${Math.ceil(spentLife / totalLife * 100)}% of your life`
+}
+setTimeout(updateProgressBar, 0);
 
 /** Search bar */
 const input = document.getElementById("searchInput");
@@ -38,3 +40,13 @@ input.addEventListener("change", function(ev) {
   const searchString = ev.target.value.trim().replace(" ", "+");
   window.open(`https://duckduckgo.com/?q=${searchString}`, '_self');
 })
+
+const searchBarDiv = document.getElementById("searchbar");
+const searchBarButton = document.getElementById("searchButton");
+searchBarDiv.addEventListener("mouseover", function() {
+  searchBarButton.style.backgroundColor = "rgb(210, 210, 210)";
+});
+
+searchBarDiv.addEventListener("mouseout", function() {
+  searchBarButton.style.backgroundColor = "rgb(239, 239, 239)"
+});
